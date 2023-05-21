12a667323f3e0a39a575dd29ac0649ef4bb2a9a2
NyasaLuharuka <73618884+NyasaLuharuka@users.noreply.github.com>
2023-05-21 13:38:14 -0500

    added waveplots and such

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import librosa
+import librosa.display
 import soundfile as sf
-import os
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import audio
 
-file = 'venv/music/rickroll.wav'
+
+song_name = "tswift"
+file = f'venv/music/{song_name}.wav'
+
 audio_array, sr = librosa.load(file)
+
+
 
 harmonic, percussive = librosa.effects.hpss(audio_array)
 
 sf.write('venv/music/harmonic.wav', harmonic, sr)
 sf.write('venv/music/percussive.wav', percussive, sr)
+
+#uses waveplot
+librosa.display.waveshow(audio_array, sr)
+plt.savefig("venv/music/output.jpg")
+
+# create new directories for each song
+# start graphing each audio file by frequencies
