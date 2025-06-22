3a4aae93259e40b6256cbb49c009afb6ca7148b4
NyasaLuharuka <73618884+NyasaLuharuka@users.noreply.github.com>
2025-06-22 15:10:27 -0400

    Add files via upload

--- FeelTheBeat.ino
+++ FeelTheBeat.ino
@@ -1,21 +1,21 @@
-// Include the Servo library 
-#include <Servo.h> 
-// Declare the Servo pin 
-int servoPin = 3; 
-// Create a servo object 
-Servo Servo1; 
-void setup() { 
-   // We need to attach the servo to the used pin number 
-   Servo1.attach(servoPin); 
+const uint8_t signal[] = {
+  127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 
+  127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 126, 124, 132, 131, 131, 
+  126, 128, 127, 126, 129, 128, 126, 129, 128, 127, 127, 127, 128, 127, 128, 128, 
+  126, 129, 127, 127, 127, 128, 126, 126, 128, 127, 127, 127, 127, 127, 127, 127
+};
+//examples of frequencies above
+int signalLength = sizeof(signal) / sizeof(signal[0]);
+
+void setup() {
+  pinMode(9, OUTPUT);
+
 }
-void loop(){ 
-   // Make servo go to 0 degrees 
-   Servo1.write(0); 
-   delay(1000); 
-   // Make servo go to 90 degrees 
-   Servo1.write(90); 
-   delay(1000); 
-   // Make servo go to 180 degrees 
-   Servo1.write(180); 
-   delay(1000); 
+
+void loop() {
+  for (int i = 0; i < signalLength; i++) {
+    analogWrite(9, signal[i]);
+    delay(10);
+  }
+
 }
